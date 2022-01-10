49dd60e72818b0c83c1c15815bfc97f906a15ee2
Daniel Oliveira <contato@danieloliveira.eti.br>
2022-01-10 11:07:49 -0300

    Strategy

---
+++ DesignPatterns.Strategy/CalculadorDeImpostos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class CalculadorDeImpostos
+    {
+        public void RealizaCalculo(Orcamento orcamento, Imposto imposto)
+        {           
+            Console.WriteLine(imposto.Calcula(orcamento));
+        }
+
+       
+    }
+}
---
+++ DesignPatterns.Strategy/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns;
+
+Imposto iss = new ISS();
+Imposto icms = new ICMS();
+
+Orcamento orcamento = new Orcamento(500.0);
+
+CalculadorDeImpostos calculador = new CalculadorDeImpostos();
+calculador.RealizaCalculo(orcamento, iss);
+calculador.RealizaCalculo(orcamento, icms); 
